121c1eb2ab10f8673902574c3b3e2cf94f9e639f
Pedro Almeida <pedro--luiz@outlook.com>
2020-12-06 20:26:09 -0300

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, optio. Dolorem, accusantium accusamus enim consequatur maxime doloremque voluptas unde inventore. Voluptatum nam iure fuga, obcaecati suscipit perspiciatis animi quae magnam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, optio. Dolorem, accusantium accusamus enim consequatur maxime doloremque voluptas unde inventore. Voluptatum nam iure fuga, obcaecati suscipit perspiciatis animi quae magnam?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
